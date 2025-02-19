ca192dce1119377f5b56c61d2fad3e29447d469f
srushti <srushtipatel4040@gmail.com>
2025-02-19 21:32:20 +0530

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import express from "express"
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials : true //option allows cookies to be sent with cross-origin requests.
 }))
 
-app.use((express.json({limit: "16kb"})))
-app.use(express.urlencoded({extented: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({limit: "16kb"})) // Middleware to parse incoming JSON requests with a size limit of 16 kilobytes. This is useful for handling JSON payloads in POST requests.
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // Middleware to parse incoming URL-encoded data (like form submissions). extended: true allows for rich objects and arrays to be encoded. The size limit is also 16 kilobytes.
+app.use(express.static("public"))  // Serves static files (like HTML, CSS, JavaScript) from the public directory. This means any files placed in the public folder can be accessed directly via their URL.
 app.use(cookieParser())
 
-export { app } 
+
+// routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+//ex. = http://localhost:8000/api/v1/users/register
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
+
 .then( () => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port : ${process.env.PORT}`)
